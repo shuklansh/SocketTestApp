3ac60a1e1128f31badf3a859379e535dec5a36a2
Ansh Shukla <ansh.shukla270501@gmail.com>
2023-07-20 14:00:58 +0530

    SocketTestApp

--- app/src/main/java/com/shuklansh/templateapp/MainActivity.kt
+++ app/src/main/java/com/shuklansh/templateapp/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.shuklansh.templateapp.ui.theme.TemplateAppTheme
+import com.shuklansh.templateapp.ui.theme.SocketTestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TemplateAppTheme {
+            SocketTestAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TemplateAppTheme {
+    SocketTestAppTheme {
         Greeting("Android")
     }
 }
